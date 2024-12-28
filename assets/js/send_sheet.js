bd6b0dba48f8c888eca3706729284d9e23c6eced
Wendel Ortiz <wendel.ortiz.dev@gmail.com>
2024-12-28 03:06:06 -0300

    send form test 2

--- assets/js/send_sheet.js
+++ assets/js/send_sheet.js
@@ -17,20 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       const response = await fetch("https://script.google.com/macros/s/AKfycbwxQx1AB4aykBUXcwnWKMFzAQxnx2lrD0PjB3z7l5hltYLzatznaCVoKjwFtSEawoJU/exec", {
         method: "POST",
+        mode: "no-cors",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, phone }),
       });
-
-      console.log(response)
-      console.log(response.json)
-      const result = await response.json();
-      console.log(result);
-      if (result.status === "success") {
-        alert("Dados enviados com sucesso!");
-        form.reset();
-      } else {
-        alert("Erro ao enviar os dados. Tente novamente.");
+    
+      console.log(response);
+      let currentUrl = window.location.href;
+      if (currentUrl.includes("index.html")) {
+          currentUrl = currentUrl.replace("index.html", "");
       }
+      const baseUrl = currentUrl.endsWith('/') ? currentUrl.slice(0, -1) : currentUrl;
+      window.location.href = baseUrl + "/starter-page.html";
+
+      // const result = await response.json();
+      // if (result.status === "success") {
+      //   alert("Dados enviados com sucesso!");
+      //   form.reset();
+      // } else {
+      //   alert("Erro ao enviar os dados. Tente novamente.");
+      // }
     } catch (error) {
       console.error("Erro:", error);
     }
